7f4585e2ef9281e8e222815551aa01940d860daf
Deeksha Sharma <84147718+drily81188@users.noreply.github.com>
2022-05-07 14:47:39 +0530

    loginPage

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+    return (
+        <div className="navbar"> 
+            <h1>emails</h1>
+            <h2>enter the email</h2>
+        </div>
+        
+    )
+    
+}
