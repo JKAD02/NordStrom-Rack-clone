985d0fe5dfba27365b4ac62ad7e4e21a7d1f7599
Jenish Kathrotiya <jenishadoasis@gmail.com>
2022-05-08 03:53:24 +0530

    navbar footer added

--- nordstrom-clone/src/App.js
+++ nordstrom-clone/src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
